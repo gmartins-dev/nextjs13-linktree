223dea3c7bc990fad308da9e4bac02bcf79c4a82
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-01-15 21:33:10 +0000

    svgs new approch

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Guilherme Martins | Frontend Developer | Links</title>
+      <meta name='description' content='My Links' />
+      <link rel='icon' href='/favicon.ico' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
     </>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,9 @@
-import Image from "next/image"
-import { get } from "@vercel/edge-config"
-import { redirect } from "next/navigation"
+import { get } from '@vercel/edge-config'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
-export const dynamic = "force-dynamic",
-  runtime = "edge"
-
-function TwitterIcon() {
-  return (
-    <svg
-      width="30"
-      height="24"
-      viewBox="0 0 24 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clipPath="url(#a)">
-        <path
-          d="M21.479 4.937c.015.209.015.418.015.628 0 6.424-4.917 13.832-13.906 13.832v-.004a13.89 13.89 0 0 1-7.491-2.18 9.847 9.847 0 0 0 7.233-2.015 4.89 4.89 0 0 1-4.566-3.375c.732.14 1.487.112 2.206-.084a4.868 4.868 0 0 1-3.92-4.764v-.062c.68.376 1.44.585 2.218.608a4.851 4.851 0 0 1-1.513-6.49 13.896 13.896 0 0 0 10.073 5.078 4.848 4.848 0 0 1 1.414-4.644 4.911 4.911 0 0 1 6.914.21A9.84 9.84 0 0 0 23.26.496a4.884 4.884 0 0 1-2.149 2.69 9.76 9.76 0 0 0 2.807-.766 9.898 9.898 0 0 1-2.439 2.518Z"
-          fill="currentColor"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M0 0h24v19.636H0z" />
-        </clipPath>
-      </defs>
-    </svg>
-  )
-}
-
-function GitHubIcon() {
-  return (
-    <svg
-      width="30"
-      height="30"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clipPath="url(#clip0_9914_10)">
-        <path
-          d="M12 0C5.374 0 0 5.373 0 12C0 17.302 3.438 21.8 8.207 23.387C8.806 23.498 9 23.126 9 22.81V20.576C5.662 21.302 4.967 19.16 4.967 19.16C4.421 17.773 3.634 17.404 3.634 17.404C2.545 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.048 18.909C9.155 18.134 9.466 17.604 9.81 17.305C7.145 17 4.343 15.971 4.343 11.374C4.343 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 15 18.397 15 19.517V22.81C15 23.129 15.192 23.504 15.801 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z"
-          fill="currentColor"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_9914_10">
-          <rect width="24" height="24" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
-  )
-}
+export const dynamic = 'force-dynamic',
+  runtime = 'edge'
 
 function LinkCard({
   href,
@@ -65,15 +17,15 @@ function LinkCard({
   return (
     <a
       href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex items-center w-full max-w-3xl p-1 mb-3 transition-all bg-gray-100 rounded-md hover:scale-105"
+      target='_blank'
+      rel='noopener noreferrer'
+      className='flex items-center w-full max-w-3xl p-1 mb-3 transition-all bg-gray-100 rounded-md hover:scale-105'
     >
-      <div className="flex w-full text-center">
-        <div className="w-10 h-10">
+      <div className='flex w-full text-center'>
+        <div className='w-10 h-10'>
           {image && (
             <Image
-              className="rounded-sm"
+              className='rounded-sm'
               alt={title}
               src={image}
               width={40}
@@ -81,7 +33,7 @@ function LinkCard({
             />
           )}
         </div>
-        <h2 className="flex items-center justify-center w-full -ml-10 font-semibold text-gray-700">
+        <h2 className='flex items-center justify-center w-full -ml-10 font-semibold text-gray-700'>
           {title}
         </h2>
       </div>
@@ -109,41 +61,55 @@ interface Social {
 }
 
 export default async function HomePage() {
-  const data: Data | undefined = await get("linktree")
+  const data: Data | undefined = await get('linktree')
 
   if (!data) {
     // not working yet https://github.com/vercel/next.js/issues/44232
-    redirect("https://linktr.ee/selenagomez")
+    redirect('https://linktr.ee/selenagomez')
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full px-8 mx-auto mt-16">
+    <div className='flex flex-col items-center justify-center w-full px-8 mx-auto mt-16'>
       <Image
         priority
-        className="rounded-full"
+        className='rounded-full'
         alt={data.name}
         src={data.avatar}
         width={96}
         height={96}
       />
-      <h1 className="mt-4 text-xl font-bold text-white">{data.name}</h1>
-      <h2 className="mb-8 font-semibold text-gray-300">{data.description}</h2>
+      <h1 className='mt-4 text-xl font-bold text-white'>{data.name}</h1>
+      <h2 className='mb-8 font-semibold text-gray-300'>{data.description}</h2>
       {data.links.map((link) => (
         <LinkCard key={link.href} {...link} />
       ))}
-      <div className="flex items-center gap-4 mt-8 text-white">
+      <div className='flex items-center gap-4 mt-8 text-white'>
         {data.socials.map((social) => (
           <a
             aria-label={`${social.title} link`}
             key={social.href}
             href={social.href}
-            target="_blank"
-            rel="noopener noreferrer"
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            {social.href.includes("twitter") ? (
-              <TwitterIcon />
-            ) : social.href.includes("github") ? (
-              <GitHubIcon />
+            {social.href.includes('twitter') ? (
+              <img
+                height='24'
+                width='24'
+                src='https://cdn.simpleicons.org/twitter/F3F4F6'
+              />
+            ) : social.href.includes('github') ? (
+              <img
+                height='24'
+                width='24'
+                src='https://cdn.simpleicons.org/github/F3F4F6'
+              />
+            ) : social.href.includes('linkedin') ? (
+              <img
+                height='24'
+                width='24'
+                src='https://cdn.simpleicons.org/linkedin/F3F4F6'
+              />
             ) : null}
           </a>
         ))}
